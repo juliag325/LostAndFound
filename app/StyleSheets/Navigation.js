d5d14a47ccef73c66e4c514d42996394b1fc4916
Alexander Rodriguez <arodriguez8303@gmail.com>
2019-11-20 13:43:23 -0500

    Added StyleSheets Folder

---
+++ app/StyleSheets/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+let styles = StyleSheet.create({
+  ButtonNav: {
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'center'
+  }
+});
+
+export {styles}
