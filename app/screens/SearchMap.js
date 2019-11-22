fea3289714d21ded988a609299105fe37c4eb039
davidmanta7 <davidmontoya1998@gmail.com>
2019-11-22 16:22:57 -0500

    Added Map and Markers

--- app/screens/SearchMap.js
+++ app/screens/SearchMap.js
@@ -1,24 +1,73 @@
-import React from 'react'; 
+import React from 'react';
 import { Button, View,Text } from 'react-native';
-import {StyleSheet} from 'react-native'; 
+import {StyleSheet} from 'react-native';
 //import MapComp from './app/components/MapComp; '
-
-class SearchMap extends React.Component { 
-    static navigationOptions = { 
+import MapView from 'react-native-maps'
+markers = [
+  { //Lib West
+    coordinate:{
+      latitude:29.6514,
+      longitude:-82.3428,
+    },
+    title:"Library West",
+    subtitle:"",
+  },
+  { //Marston
+    coordinate:{
+      latitude:29.6481,
+      longitude:-82.3442,
+    },
+    title:"Marston Science Library",
+    subtitle:"",
+  },
+  { //Broward Hall
+    coordinate:{
+      latitude:29.6465,
+      longitude:-82.3420,
+    },
+    title:"Broward Hall",
+    subtitle:"",
+  },
+  { //Reitz Union
+    coordinate:{
+      latitude:29.6463,
+      longitude:-82.3478,
+    },
+    title:"Reitz Union",
+    subtitle:"",
+  },
+  { //New Engineering Building
+    coordinate:{
+      latitude:29.6422,
+      longitude:-82.3471,
+    },
+    title:"New Engineering Building",
+    subtitle:"",
+  }
+]
+class SearchMap extends React.Component {
+    static navigationOptions = {
       title: 'Have You Seen It?'
-    }; 
-render() { 
+    };
+render() {
     return (
-        <View style={{ 
-         flex: 1,
-         alignItems:'center',
-         justifyContent:'center'
-        }}>
-      <Button title="Go to Home screen"
-          onPress={() => this.props.navigation.navigate('Home')}
-         />
+      <View style={{flex:1}}>
+        <MapView style={{flex:1}}
+        ref={map => this.map = map}
+        region = {{latitude:29.6463, longitude:-82.3528, latitudeDelta: 0.0382, longitudeDelta: 0.0382}}
+        showsUserLocation={true}>
+        {markers.map((marker,index) => {
+          return (
+            <MapView.Marker key = {index} coordinate={marker.coordinate}>
+            </MapView.Marker>
+          );
+        })}
+        </MapView>
+        <Button title="Go to Home screen"
+            onPress={() => this.props.navigation.navigate('Home')}
+        />
         </View>
       );
+    }
 }
-}
-export default SearchMap; 
+export default SearchMap;
