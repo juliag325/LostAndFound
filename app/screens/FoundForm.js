441e3eaa3579d64358a857bea3d246bbb70fc7d7
Alexander Rodriguez <arodriguez8303@gmail.com>
2019-11-23 18:58:40 -0500

    Update FoundForm.js

--- app/screens/FoundForm.js
+++ app/screens/FoundForm.js
@@ -1,24 +1,68 @@
-import React from 'react'; 
-import { Button, View,Text } from 'react-native';
-import {StyleSheet} from 'react-native'; 
+import React from 'react';
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 
-class FoundForm extends React.Component { 
-    static navigationOptions = { 
-      title: 'Found Form'
-    }; 
+class FoundForm extends React.Component {
+  state = {
+      email: '',
+      password: ''
+   }
+   handleEmail = (text) => {
+      this.setState({ email: text })
+   }
+   handlePassword = (text) => {
+      this.setState({ password: text })
+   }
+   submit = (email, pass) => {
+      alert('email: ' + email + ' password: ' + pass)
+   }
+   render() {
+      return (
+         <View style = {styles.container}>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Item"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handleEmail}/>
 
-    render() { 
-        return (
-            <View style={{ 
-             flex: 1,
-             alignItems:'center',
-             justifyContent:'center'
-            }}>
-          <Button title="Home"
-              onPress={() => this.props.navigation.navigate('Home')}
-             />
-            </View>
-          );
-    }
-    }
-    export default FoundForm; 
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Description"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.submit(this.state.email, this.state.password)
+               }>
+               <Text style = {styles.submitButtonText}> Submit </Text>
+            </TouchableOpacity>
+         </View>
+      )
+   }
+}
+
+export default FoundForm;
+
+const styles = StyleSheet.create({
+   container: {
+      paddingTop: 23
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#7a42f4',
+      borderWidth: 1
+   },
+   submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+   },
+   submitButtonText:{
+      color: 'white'
+   }
+})
