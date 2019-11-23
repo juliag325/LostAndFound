8e6e56f0d78059c337bfd57b0bb34abc0461269a
Alexander Rodriguez <arodriguez8303@gmail.com>
2019-11-23 15:49:39 -0500

    Update item.js

--- data/models/item.js
+++ data/models/item.js
@@ -2,10 +2,9 @@ import mongoose, { Schema } from 'mongoose';
 
 // Define movie schema
 var itemSchema = new Schema({
-  name: {
-    type: String,
-    unique: true,
-  },
+  name: String,
+  _found: Date,
+  category: String,
   desc: String
 });
 
