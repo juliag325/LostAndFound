0c21a2b906c3b37040dc3ab97a20040f1fda9478
Alexander Rodriguez <arodriguez8303@gmail.com>
2019-11-24 16:21:04 -0500

    staff broke before

--- app/screens/Staff.js
+++ app/screens/Staff.js
@@ -50,50 +50,42 @@ const st = StyleSheet.create({
 class Staff extends React.Component {
     static navigationOptions = {
       title: 'Staff'
-    };
+    }; 
+render() {
+    return (
+        <View style={{
+         flex: 1,
+         alignItems:'center',
+         justifyContent:'center',
+         backgroundColor: '#ADD8E6',
+         fontFamily: 'Georgia'
+        }}>
+      <Text style = {st.title}>Staff Sign In</Text>
+         <Text></Text>
 
-    state = {
-        item: '',
-        description: '',
-        date: '',
-        category: `${this.buttons[1]}`
-     }
+      <Text style = {st.baseText}>Location ID</Text>
+      <TextInput style = {st.input}
+        underlineColorAndroid = "transparent"
+        placeholder = "Location ID"
+        placeholderTextColor = "#696969"
+        onChangeText = {this.handleDesc}/>
+      <Text></Text>
 
-    render() {
-      return (
-          <View style={{
-           flex: 1,
-           alignItems:'center',
-           justifyContent:'center',
-           backgroundColor: '#ADD8E6',
-           fontFamily: 'Georgia'
-          }}>
-        <Text style = {st.title}>Staff Sign In</Text>
-           <Text></Text>
+      <Text style = {st.baseText}>Staff ID</Text>
+      <TextInput style = {st.input}
+        underlineColorAndroid = "transparent"
+        placeholder = "Staff ID"
+        placeholderTextColor = "#696969"
+        onChangeText = {this.handleDesc}/>
 
-        <Text style = {st.baseText}>Location ID</Text>
-        <TextInput style = {st.input}
-          underlineColorAndroid = "transparent"
-          placeholder = "Location ID"
-          placeholderTextColor = "#696969"
-          onChangeText = {this.handleDesc}/>
-        <Text></Text>
-
-        <Text style = {st.baseText}>Staff ID</Text>
-        <TextInput style = {st.input}
-          underlineColorAndroid = "transparent"
-          placeholder = "Staff ID"
-          placeholderTextColor = "#696969"
-          onChangeText = {this.handleDesc}/>
-
-        <TouchableOpacity
-          style = {st.searchButton}
-          onPress={() => this.props.navigation.navigate('WelcomeBack')}
-          underlayColor='#fff'>
-            <Text style = {st.loginText}>Login</Text>
-        </TouchableOpacity>
-          </View>
-        );
-    }
+      <TouchableOpacity
+        style = {st.searchButton}
+        onPress={() => this.props.navigation.navigate('WelcomeBack')}
+        underlayColor='#fff'>
+          <Text style = {st.loginText}>Login</Text>
+      </TouchableOpacity>
+        </View>
+      );
+}
 }
 export default Staff;
