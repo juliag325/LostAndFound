a447f261ea6a2e21fb0414a9abf2f54f2c3c02f2
Swathi Chittlur <swathi.chittlur@gmail.com>
2019-11-23 21:33:46 -0500

    Update Staff.js
    Staff sign in

--- app/screens/Staff.js
+++ app/screens/Staff.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, View,Text } from 'react-native';
 import {StyleSheet} from 'react-native'; 
 import { TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler';
+import { TextInput } from 'react-native';
 
 const st = StyleSheet.create({ 
   title: {
@@ -13,6 +14,10 @@ const st = StyleSheet.create({
   },
   background: { 
     color: 'blue',
+  },
+  baseText:{
+    color: 'black',
+    fontSize: 20
   },
   searchButton: { 
    marginRight: 40,
@@ -26,7 +31,13 @@ const st = StyleSheet.create({
    borderColor: '#fff',
    fontSize: 20
   },
-
+  input: {
+    margin: 15,
+    height: 40,
+    width: 300,
+    borderColor: '#696969',
+    borderWidth: 1
+ },
   loginText: { 
     color: '#fff',
     textAlign: 'center',
@@ -49,36 +60,30 @@ render() {
          backgroundColor: '#ADD8E6',
          fontFamily: 'Georgia'
         }}>
-      <Text style = {st.title}>Welcome Back!</Text>
+      <Text style = {st.title}>Staff Sign In</Text>
          <Text></Text>
       
-      <TouchableOpacity 
-        style = {st.searchButton}
-        onPress={() => this.props.navigation.navigate('FoundForm')}
-        underlayColor='#fff'>
-          <Text style = {st.loginText}>Found Form</Text>
-      </TouchableOpacity>
+      <Text style = {st.baseText}>Location ID</Text>
+      <TextInput style = {st.input}
+            underlineColorAndroid = "transparent"
+            placeholder = "Location ID"
+            placeholderTextColor = "#696969"
+            onChangeText = {this.handleDesc}/>
+      <Text></Text>
 
+      <Text style = {st.baseText}>Staff ID</Text>
+      <TextInput style = {st.input}
+            underlineColorAndroid = "transparent"
+            placeholder = "Staff ID"
+            placeholderTextColor = "#696969"
+            onChangeText = {this.handleDesc}/>
+      
       <TouchableOpacity 
-        style = {st.searchButton}
-        onPress={() => this.props.navigation.navigate('Pickup')}
-        underlayColor='#fff'>
-          <Text style = {st.loginText}>Pickup</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity 
-        style = {st.searchButton}
-        onPress={() => this.props.navigation.navigate('History')}
-        underlayColor='#fff'>
-          <Text style = {st.loginText}>History</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity 
-        style = {st.searchButton}
-        onPress={() => this.props.navigation.navigate('Home')}
-        underlayColor='#fff'>
-          <Text style = {st.loginText}>Log Out</Text>
-      </TouchableOpacity>
+               style = {st.searchButton}
+               onPress={() => this.props.navigation.navigate('WelcomeBack')}
+               underlayColor='#fff'>
+                 <Text style = {st.loginText}>Found Form</Text>
+             </TouchableOpacity>
         </View>
       );
 }
